906bbbbf82fd560ad5187a0a6dde4579641857f2
architolk <msj@xs4all.nl>
2022-12-01 23:39:59 +0100

    Eerste versie LD conversie

---
+++ model/mim-ld-export/convert.sh
@@ -0,0 +1,20 @@
+# Conversion of MIM UML to MIM-LD
+# Used for local conversion
+# Use pipeline version for automatic conversion in github
+# Dependencies:
+# - https://github.com/architolk/ea2rdf
+# - https://github.com/architolk/rdf2rdf
+# - https://github.com/architolk/mimtools
+#
+
+# First step: transform native EAP format to RDF
+java -jar ~/GITREPO/ea2rdf/target/ea2rdf.jar -ea -e "../nen3610-2022-template - 20221116.eap" > nen3610-2022-ea.ttl
+
+# Second step: transform EA model in RDF to MIM in RDF
+java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-ea.ttl nen3610-2022-mim.ttl ~/GITREPO/mimtools/ea2mim.yaml
+
+# Third step: transform MIM model in RDF to RDFS/OWL/SHACL ontology
+java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-mim.ttl nen3610-2022-ont.ttl ~/GITREPO/mimtools/mim2onto.yaml
+
+# Fourth step: create diagram from ontology
+java -jar ~/GITREPO/rdf2xml/target/rdf2xml.jar nen3610-2022-ont.ttl nen3610-2022-model.graphml ~/GITREPO/rdf2xml/rdf2graphml.xsl
