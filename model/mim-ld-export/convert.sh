61eb38558433c8ee9d3820f63b6cb83091b0c682
architolk <msj@xs4all.nl>
2022-12-09 13:57:56 +0100

    New version of ontology

--- model/mim-ld-export/convert.sh
+++ model/mim-ld-export/convert.sh
@@ -8,13 +8,19 @@
 #
 
 # First step: transform native EAP format to RDF
-java -jar ~/GITREPO/ea2rdf/target/ea2rdf.jar -ea -e "../nen3610-2022-template - 20221116.eap" > nen3610-2022-ea.ttl
+#java -jar ~/GITREPO/ea2rdf/target/ea2rdf.jar -ea -e "../nen3610-2022-template - 20221116.eap" > nen3610-2022-ea.ttl
 
 # Second step: transform EA model in RDF to MIM in RDF
-java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-ea.ttl nen3610-2022-mim.ttl ~/GITREPO/mimtools/ea2mim.yaml
+java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-ea.ttl nen3610-2022-mim-all.ttl ~/GITREPO/mimtools/ea2mim.yaml
 
-# Third step: transform MIM model in RDF to RDFS/OWL/SHACL ontology
+# Third step: get only the NEN3610 Informatimodel from the EA model
+java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-mim-all.ttl nen3610-2022-mim.ttl nen3610-split.yaml
+
+# Fourth step: transform MIM model in RDF to RDFS/OWL/SHACL ontology
 java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-mim.ttl nen3610-2022-ont.ttl ~/GITREPO/mimtools/mim2onto.yaml
 
-# Fourth step: create diagram from ontology
-java -jar ~/GITREPO/rdf2xml/target/rdf2xml.jar nen3610-2022-ont.ttl nen3610-2022-model.graphml ~/GITREPO/rdf2xml/rdf2graphml.xsl
+# Fifth step: create diagram from ontology
+#java -jar ~/GITREPO/rdf2xml/target/rdf2xml.jar nen3610-2022-ont.ttl nen3610-2022-model.graphml ~/GITREPO/rdf2xml/rdf2graphml.xsl
+
+# Sixth step: specific NEN3610 steps
+java -jar ~/GITREPO/rdf2rdf/target/rdf2rdf.jar nen3610-2022-ont.ttl nen3610-2022-final.ttl nen3610.yaml nen3610-2022-mim.ttl
